8fb90b82ac862911f538c4bf3e281d0fcb8fbecc
sachins <sachins@hobbyist.co.nz>
2020-06-04 10:44:55 +1200

    Remote call to NN and returns results

---
+++ src/api/seed.go
@@ -0,0 +1,3 @@
+package api
+
+var seed_for_rand int64 = 123
--- src/client/httpClient.go
+++ src/client/httpClient.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api"
 	"context"
 	"fmt"
 	"proto"
@@ -9,76 +10,50 @@ import (
 	"google.golang.org/grpc"
 )
 
+//For generating boards for test, remove later
+func setupGame(game *api.Connect4, moves []int) *api.Connect4 {
+
+	for _, move := range moves {
+
+		//fmt.Println("===============================")
+		//fmt.Printf("Player to move %s\n", game.PlayerToString(game.GetPlayerToMove()))
+		game.PlayMove(move)
+		//fmt.Printf("Player who just Moved %s\n", game.PlayerToString(game.GetPlayerWhoJustMoved()))
+	}
+	//fmt.Printf("Board Index = %s", game.GetBoardIndex().String())
+	return game
+
+}
+
 func main() {
+	var game = api.NewConnect4()
+	moves := []int{1, 0, 2, 4, 3, 0, 6, 1, 3, 2, 4, 6, 2, 0, 4, 1, 6, 3, 0, 4, 5, 3, 6, 3} //, 6, 2}
+	game = setupGame(game, moves)
+
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
 
 	client := proto.NewAddServiceClient(conn)
-	a := []int32{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	a := game.GetBoardFlatInt32()
+	fmt.Println("a = %v", a)
 	//var b int = 50
 	req := &proto.BoardRequest{Board: a}
+	deadlineMs := 10000
+	//Increase timeout as the server on init takes few secs to return ( its loading nn into GPU memory)
+	clientDeadline := time.Now().Add(time.Duration(deadlineMs) * time.Millisecond)
+	ctx, _ := context.WithDeadline(context.Background(), clientDeadline)
 
-	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	//ctx, _ := context.WithTimeout(context.Background(), time.Second)
 
 	response, err := client.ForwardPass(ctx, req)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
+
 	for _, i := range response.Result {
 		fmt.Printf(" Response %f\n", float32(i))
 	}
 
-	/*
-
-		g := gin.Default()
-		g.GET("/add/:a/:b", func(ctx *gin.Context) {
-			a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-			if err != nil {
-				ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-				return
-			}
-			b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-			if err != nil {
-				ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-				return
-			}
-			req := &proto.Request{A: int64(a), B: int64(b)}
-			if response, err := client.Add(ctx, req); err == nil {
-				ctx.JSON(http.StatusOK, gin.H{
-					"result": fmt.Sprint(response.Result),
-				})
-			} else {
-				ctx.JSON(http.StatusInternalServerError, gin.H{
-					"error": err.Error(),
-				})
-			}
-		})
-		/*
-			g.GET("/subtract/:a/:b", func(ctx *gin.Context) {
-				a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-				if err != nil {
-					ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-					return
-				}
-				b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-				if err != nil {
-					ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-					return
-				}
-				req := &proto.Request{A: int64(a), B: int64(b)}
-				if response, err := client.Subtract(ctx, req); err == nil {
-					ctx.JSON(http.StatusOK, gin.H{
-						"result": fmt.Sprint(response.Result),
-					})
-				} else {
-					ctx.JSON(http.StatusInternalServerError, gin.H{
-						"error": err.Error(),
-					})
-				}
-			})
-		if err := g.Run(":8080"); err != nil {
-			log.Fatalf("Failed to run server: %v", err)
-		}*/
 }
